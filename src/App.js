fe690cb02b1d0123fef13d6bcd9f62af3588d303
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-02 13:57:45 +0200

    initial data for apis

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import { DateTime } from "luxon";
 const { REACT_APP_API_KEY } = process.env;
 console.log(REACT_APP_API_KEY)
 
+const IP = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_6AydnzJU6xGcHqz1YDMdcoXQofx0E&ipAddress=8.8.8.8'
+const country_api = 'https://countries.trevorblades.com/'
+const body = {
+    "query": "{\n  country(code: \"DE\") {\n    name\n    native\n    emojiU\n    languages {\n      name\n    }\n    continent {\n      name\n    }\n  }\n}\n"
+}
+const query = `
+{ country(code: "DE") {
+
+    name
+    native
+    emojiU
+    languages {
+      name
+    }
+    continent {
+      name
+    }
+  
+    }}
+`
+
+const flag_url = 'https://flagcdn.com/160x120/za.png'
+
 function App() {
   return (
     <div className="App">
