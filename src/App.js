b76efe0064773776e6470a08ca09cc91b63bc417
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-02 15:25:52 +0200

    refactor into multiple useEffects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 const { REACT_APP_API_KEY } = process.env;
 console.log(REACT_APP_API_KEY);
 
-const IP_api = (key) => 
+const IP_api = (key) =>
     "https://geo.ipify.org/api/v2/country,city?apiKey=" + key;
 
 const country_api = "https://countries.trevorblades.com/";
@@ -30,28 +30,52 @@ const query = `
     }}
 `;
 
-const flag_url = "https://flagcdn.com/160x120/za.png";
+const flag_url = (code) => `https://flagcdn.com/160x120/${code}.png`;
 
 function App() {
     const [ipData, setIpData] = useState();
-    
+
+    const [code, setCode] = useState();
+
+    const [countryData, setCountryData] = useState();
+
     useEffect(() => {
-        fetch(IP_api(REACT_APP_API_KEY))
-            .then((response) => response.json())
-            .then((json) => {
-                setIpData(json);
+        const interval = setInterval(
+            () =>
+                fetch(IP_api(REACT_APP_API_KEY))
+                    .then((response) => response.json())
+                    .then((json) => {
+                        setIpData(json);
+                        setCode(json.location.country.toLowerCase());
+                    }),
+            30_000
+        );
 
-                const code = json.location.country;
-
-                console.log(code);
-
-                axios.post(country_api, {
-                    query,
-                }).then(console.log)
-            });
+        return () => clearInterval(interval)
     }, []);
 
-    return <div className="App">{JSON.stringify(ipData)}</div>;
+    useEffect(() => {
+        if (code) {
+            axios
+                .post(country_api, {
+                    query,
+                })
+                .then((data) => {
+                    setCountryData(data.data);
+                    console.log(countryData);
+                });
+        }
+    }, [code]);
+
+    return (
+        <div className="App">
+            <div>{JSON.stringify(ipData)}</div>
+
+            <div>{JSON.stringify(countryData)}</div>
+
+            {ipData && <img src={flag_url(code)}></img>}
+        </div>
+    );
 }
 
 export default App;
