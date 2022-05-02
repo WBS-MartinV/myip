671dfff0ad26235ce36d8d42c1da5b626e28101c
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-02 13:47:16 +0200

    Co-authored-by: kiongxiangyi <kiongxiangyi@users.noreply.github.com> Co-authored-by: Anton Litvak <natlit2@users.noreply.github.com> Co-authored-by: theactofshining <Barrikoder@users.noreply.github.com> Co-authored-by: eYanmaz <yanmaz4@users.noreply.github.com> Co-authored-by: tlvu93 <tlvu93@users.noreply.github.com> Co-authored-by: Isaiah-Umuks <Isaiah-Umuks@users.noreply.github.com> Co-authored-by: r-ph-bollin <r-ph-bollin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Map, Marker } from "pigeon-maps"
 import { DateTime } from "luxon";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY)
+const { REACT_APP_API_KEY } = process.env;
+console.log(REACT_APP_API_KEY)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Hello!
     </div>
   );
 }
